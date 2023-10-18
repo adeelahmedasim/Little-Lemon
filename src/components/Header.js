442f4dd8f63fe837bde5425d96d017ace18145ea
Adeel Ahmed Asim <ahmedadeel44@gmail.com>
2023-10-18 21:54:20 +0500

    Add Sementic HTML and Meta Tags

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <img src="assets\Logo.png" alt="Logo" />
+     <nav>
+<ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Menu</li>
+    <li>Reservations</li>
+    <li>Order Online</li>
+</ul>
+     </nav>
+     <img src="assets\cart.png" alt="cart" />
+     <img src="assets\profile.png" alt="profile" />
+    </header>
+  )
+}
