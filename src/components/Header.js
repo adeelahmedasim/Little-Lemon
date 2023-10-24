51c229163c597c9111114922bb58008840e95ac3
Adeel Ahmed Asim <ahmedadeel44@gmail.com>
2023-10-24 23:30:15 +0500

    Navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,6 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-        <img src="assets\Logo.png" alt="Logo" />
-     <nav>
-<ul>
-    <li>Home</li>
-    <li>About</li>
-    <li>Menu</li>
-    <li>Reservations</li>
-    <li>Order Online</li>
-</ul>
-     </nav>
-     <img src="assets\cart.png" alt="cart" />
-     <img src="assets\profile.png" alt="profile" />
     </header>
   )
 }
